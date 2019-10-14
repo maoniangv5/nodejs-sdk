c5f959053df28035e12f179a3feea085dc661187
maoniangv5 <cpmy1223@163.com>
2019-10-14 10:12:54 +0800

    wonderscloud

--- qiniu/zone.js
+++ qiniu/zone.js
@@ -4,7 +4,7 @@ const conf = require('./conf');
 
 //huadong
 exports.Zone_z0 = new conf.Zone([
-    'up.qiniup.com',
+    'ptest.wonderscloud.com/nodefastdfs/openapi/files/upload/',
     'up-nb.qiniup.com',
     'up-xs.qiniup.com',
 ], [
